0f341b2032df4584d8a746771440aa469691eafc
ancamariac <ciuciuancamaria@gmail.com>
2021-12-04 15:59:15 +0200

    Documented code

--- maze.v
+++ maze.v
@@ -3,11 +3,11 @@
 module maze(
 input 		clk,
 input [maze_width - 1:0] 	starting_col, starting_row, 	// indicii punctului de start
-input 		maze_in, 												// ofera informa?ii despre punctul de coordonate [row, col]
+input 		maze_in, 												// ofera informatii despre punctul de coordonate [row, col]
 output reg [maze_width - 1:0] row, col, 						// selecteaza un rând si o coloana din labirint
-output reg	maze_oe,													// output enable (activeaza citirea din labirint la rândul ?i coloana date) - semnal sincron		
-output reg	maze_we, 												// write enable (activeaza scrierea în labirint la rândul ?i coloana date) - semnal sincron
-output reg	done);		 											// ie?irea din labirint a fost gasita; semnalul ramane activ 
+output reg	maze_oe,													// output enable (activeaza citirea din labirint la rândul si coloana date) - semnal sincron		
+output reg	maze_we, 												// write enable (activeaza scrierea în labirint la rândul si coloana date) - semnal sincron
+output reg	done);		 											// iesirea din labirint a fost gasita; semnalul ramane activ 
 
 	parameter maze_width  = 6;
 	parameter state_width = 4;
@@ -27,8 +27,6 @@ output reg	done);		 											// ie?irea din labirint a fost gasita; semnalul r
 	`define orientation	            1	
 	`define move	                  2	
 	`define mission_accomplished	   3
-	
-
 
 	// sequential part
 	always @(posedge clk)begin
@@ -64,24 +62,26 @@ output reg	done);		 											// ie?irea din labirint a fost gasita; semnalul r
 			maze_oe = 1;
 			next_state = `move;
 			
+			// aici se realizeaza rotirea de 90 de grade in cazul in care
+			// va fi detectat peretele
 			case (direction)				
-			
-				UP : begin         // next up
+						
+				UP : begin         
 					direction = LEFT;
 					row = row - 1;
 				end
 				
-				LEFT : begin         // next left
+				LEFT : begin        
 					direction = DOWN;
 					col = col - 1;
 				end
 
-				DOWN : begin         // next down
+				DOWN : begin       
 					direction = RIGHT;
 					row = row + 1;
 				end
 					
-				RIGHT : begin         // next right
+				RIGHT : begin       
 					direction = UP;
 					col = col + 1;
 				end
@@ -90,29 +90,35 @@ output reg	done);		 											// ie?irea din labirint a fost gasita; semnalul r
 			
 		end
 		
-		`move : begin		
+		`move : begin
+
 			next_state = `orientation;
-			  
-			if (maze_in == 1) begin		//ma intorc daca dau in perete
+			
+			// verificarea efectiva a unui potential blocaj
+			if (maze_in == 1) begin	
 				
 				row = last_row;
 				col = last_col;
 				
 		   end else begin		
-			
+				// marcarea drumului neblocat
 				maze_we = 1;
 				
-			   if (last_col - 1 ==col && last_row == row) begin		//vin din dreapta ma uit in sus
-					direction = 0;
+				// directia de mers: dreapta->stanga => rotire 90 dreapta => noua directie e UP
+			   if (last_col - 1 == col && last_row == row) begin		
+					direction = UP;
 				end
-				if (last_row + 1 == row && last_col == col) begin		//vin de sus ma uit la stanga
-					direction = 1;
+				// directia de mers: sus->jos => rotire 90 dreapta => noua directie e LEFT
+				if (last_row + 1 == row && last_col == col) begin		
+					direction = LEFT;
 				end
-			   if (last_col + 1 == col && last_row == row) begin		//vin din stanga ma uit in jos
-				   direction = 2;
+				// directia de mers: stanga->dreapta => rotire 90 dreapta => noua directie e DOWN
+			   if (last_col + 1 == col && last_row == row) begin		
+				   direction = DOWN;
 				end
-			   if (last_row - 1 == row && last_col == col) begin		//vin de jos la uit la dreapta
-				   direction = 3;
+				// directia de mers: jos->sus => rotire 90 dreapta => noua directie e RIGHT
+			   if (last_row - 1 == row && last_col == col) begin		
+				   direction = RIGHT;
 				end
 			   if (col <= 0 || col >= 63 || row <= 0 || row >= 63) begin
 				   next_state = `mission_accomplished;
